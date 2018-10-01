f94d657c10d0a6df6190d847ed9fd35193b7ec41
simone <simone.mapelli@gmail.com>
2018-10-01 15:09:08 +0200

    Using Loader | Empty ListView with no data/wrong url | re-commit after reset

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -1,105 +1,146 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+// Loader lib stuff
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity  implements LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    private ArrayList<Earthquake> earthquakes;
+    private List<Earthquake> earthquakes;
+    // Empty view
+    private TextView emptyListText;
+
+    // ListView Adapter
+    private EarthquakeAdapter adapter;
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
+    // Constant  id for loader which retrieve data from remote source (not necessary there is only it!)
+    private static final int EARTHQUAKE_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-
-        AsyncTask<String, Void, ArrayList<Earthquake>> task = new EarthquakeFetchDataAsyncTask(this).execute(USGS_REQUEST_URL);
-
-    }
-
-
-    /**
-     * Used for create/update UI when data will be available
-     */
-    protected void updateListView(){
         // Find a reference to the {@link ListView} in the layout : using listView because it has only tens of
         // entry, otherwise RecycleView would be better
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link EarthquakeAdapter} of {@link Earthquakes} objects
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        adapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
+        // Set the adapter on the {@link ListView} so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
 
+        // set Empty View in case of List empty
+        emptyListText = findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(emptyListText);
+
+
+
+        // Click on item
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // get url data
-                // 1st method : don't like it
-                /*
-                Earthquake earthquake = earthquakes.get(position);
-                String url = earthquake.getUrl();
-                */
-                // 2nd method : better
-                Earthquake earthquake = earthquakes.get(position);
 
+                // get url data
+                Earthquake earthquake = earthquakes.get(position);
                 String url = earthquake.getUrl();
                 Log.i("setOnItemClickListener", "onItemClick: "+url);
+
                 // Open the related url page of the eq clicked
                 Uri webpage = Uri.parse(url);
                 Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
                 startActivity(Intent.createChooser(webIntent, "Open details"));
             }
         });
+
+
+        // LoaderManager reference
+        LoaderManager loaderManager = getLoaderManager();
+        // Init loader : id above, bundle = null , this= current activity for LoaderCallbacks
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
+    }
+
+
+
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int id, Bundle args) {
+        Log.i(LOG_TAG, "onCreateLoader: Create a new Loader");
+        // create a new loader for the url
+        return new EarthquakeAsyncLoader(this, USGS_REQUEST_URL );
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakesReturnedByLoader) {
+        Log.i(LOG_TAG, "onLoadFinished: Loader return back with data");
+        // Set empty state text to display "No earthquakes found."
+        emptyListText.setText(R.string.no_earthquakes);
+
+        // clear the adapater of previous data
+        adapter.clear();
+
+        // update UI when loader finishes its job
+        if (setEartquakesList(earthquakesReturnedByLoader)==true) {
+            adapter.addAll(earthquakes);
+        } else {
+            Log.i(LOG_TAG, "Problem with earthquake list, is empty. Check the request. ");
+        }
+
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+        Log.i(LOG_TAG, "onLoaderReset: Reset Loader previous data");
+        // reset loader to clean up previous data
+        adapter.clear();
     }
 
 
 
     /**
-     * Used by {@link EarthquakeFetchDataAsyncTask  } onPostExecutemethod to populate the ArrayList fetched
+     * Used by onLoadFinished to populate the ArrayList fetched
      * @param earthquakes
+     * @return true/false
      */
-    protected void setEartquakesList(ArrayList<Earthquake> earthquakes){
+    protected boolean setEartquakesList(List<Earthquake> earthquakes){
         if (  (earthquakes != null) && (earthquakes.isEmpty() == false)  ){
             this.earthquakes = earthquakes;
+            return true;
         }else{
             Log.i(LOG_TAG, "The earthquake list is empty. Check the request. ");
-            Toast.makeText(this, "The earthquake list is empty. Check the request. ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "The earthquake list is empty. Check the request. ", Toast.LENGTH_LONG).show();
+            return false;
         }
     }
+
+
+
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeAsyncLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAsyncLoader.java
@@ -1,8 +1,8 @@
+package com.example.android.quakereport;
+
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.util.Log;
-
-import com.example.android.quakereport.Earthquake;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,7 +37,7 @@ public class EarthquakeAsyncLoader extends AsyncTaskLoader<List<Earthquake>> {
     /**
      * Loader constructor, pass the
      * @param context  : context of the activity
-     * @param url  : url to be queried
+     * @param url  : url to be queried for
      */
     public EarthquakeAsyncLoader(Context context, String url){
         super(context);
@@ -46,6 +46,7 @@ public class EarthquakeAsyncLoader extends AsyncTaskLoader<List<Earthquake>> {
 
     @Override
     protected void onStartLoading() {
+        Log.i(LOG_TAG, "onStartLoading: forceLoad on this loader.");
         forceLoad();
     }
 
@@ -59,147 +60,14 @@ public class EarthquakeAsyncLoader extends AsyncTaskLoader<List<Earthquake>> {
         if(queryUrl == null){
             return null;
         }
-        return extractFeatureFromJson(queryUrl);
-    }
-
-
-
-
-    //** METHODS USED INSIDE LOADER CALLBACK for retrieving data from RESTFull service
-    // TODO : PUT IT IN A separate CLASS
-
-    /**
-     * Returns new URL object from the given string URL.
-     */
-    private URL createUrl(String stringUrl) {
-        URL url = null;
-        try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Error with creating URL", e);
-            return null;
-        }
-        return url;
-    }
-
-    /**
-     * Make an HTTP request to the given URL and return a String as the response.
-     */
-    private String makeHttpRequest(URL url) throws IOException {
-        String jsonResponse = "";
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            // check http response before doing anything
-            Log.i(LOG_TAG, "makeHttpRequest: check response code");
-            int responseCode = checkHttpResponse(urlConnection);
-            if (responseCode==200) {
-                urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            }else{
-                Log.e(LOG_TAG, "Response code not 200 : "+responseCode);
-                jsonResponse = "";
-            }
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (inputStream != null) {
-                // function must handle java.io.IOException here
-                inputStream.close();
-            }
-        }
-        return jsonResponse;
-    }
-
-    /**
-     * Convert the {@link InputStream} into a String which contains the
-     * whole JSON response from the server.
-     */
-    private String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-        return output.toString();
-    }
-
-
-
-    /**
-     * Return an {@link Earthquake} object by parsing out information
-     * about the first earthquake from the input earthquakeJSON string.
-     *
-     * Return the list of earthquake retrieved from remote
-     */
-    private ArrayList<Earthquake> extractFeatureFromJson(String earthquakeJSON) {
-        // Create an empty ArrayList that we can start adding earthquakes to
-        earthquakes = new ArrayList<>();
-
-        // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
-        // is formatted, a JSONException exception object will be thrown.
-        // Catch the exception so the app doesn't crash, and print the error message to the logs.
-        try {
-
-            // build up a list of Earthquake objects with the corresponding data.
-            JSONObject jsonObject = new JSONObject(earthquakeJSON);
-            JSONArray features  = jsonObject.getJSONArray("features");
-
-            for(int i=0;i<features.length();i++){
-                // get current earthquake record
-                JSONObject currentEarthquake = features.getJSONObject(i);
-                // get properties obj for current eq
-                JSONObject properties = currentEarthquake.getJSONObject("properties");
-
-                double mag   = properties.getDouble("mag");
-                String place = properties.getString("place");
-                long time    = properties.getLong("time");
-                String url   = properties.getString("url");
-
-                // tmp earthquake obj for single item
-                Earthquake  tmp = new Earthquake(mag, place, time, url);
-
-                // add to list of all earthquakes
-                earthquakes.add(tmp);
-            }
-
-        } catch (JSONException e) {
-            // If an error is thrown when executing any of the above statements in the "try" block,
-            // catch the exception here, so the app doesn't crash. Print a log message
-            // with the message from the exception.
-            Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
-        }
-
-        // Return the list of earthquakes
+        // create instance of request and collect the result
+        earthquakes = new EarthQuakeQuery().fetchEarthquakeData(queryUrl);
+        Log.i(LOG_TAG, "loadInBackground: loadInBackground ended, returning data requested.");
         return earthquakes;
     }
 
 
-    /**
-     * Return http response code
-     * @param urlConnection
-     * @return
-     */
-    private int checkHttpResponse(HttpURLConnection urlConnection){
-        int responseCode = 0;
-        try {
-            responseCode = urlConnection.getResponseCode();
-        }catch(IOException e){
-            Log.e(LOG_TAG+".checkHttpResponse", "Problem getting HTTP response.", e);
-        }
-        return responseCode;
-    }
+
+
+
 }
