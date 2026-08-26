91c1588b7a5247d4365ff646b77e4fa997a6dc75
simone <simone.mapelli@abenergie.it>
2026-08-26 19:43:02 +0200

    fix: close repository hygiene gaps

--- scripts/verify_repository_hygiene.sh
+++ scripts/verify_repository_hygiene.sh
@@ -13,7 +13,7 @@ report_failure() {
 check_tracked_files() {
     local matches
 
-    if matches=$(git ls-files | grep -E '(^|/)(google-services\.json|keystore\.properties|[^/]*(service[-_]?account|serviceaccount)[^/]*\.json|benchmarkData\.json)$|(\.(jks|keystore|p12|apk|aab|apks|dm|trace|perfetto-trace))$'); then
+    if matches=$(git ls-files | grep -E '(^|/)(google-services\.json|google_maps_api\.xml|admob_key\.xml|ads_key_ids\.xml|keystore\.properties|[^/]*(service[-_]?account|serviceaccount)[^/]*\.json|benchmarkData\.json|output-metadata\.json)$|(\.(jks|keystore|p12|apk|aab|apks|ap_|dm|trace|perfetto-trace))$'); then
         report_failure "tracked secret or generated artifact" "$matches"
     fi
 }
@@ -21,9 +21,76 @@ check_tracked_files() {
 check_executable_source() {
     local matches
 
-    # Only full-line comments are exempt; stale code inside block comments remains visible.
-    matches=$(rg -n --glob '*.kt' --glob '*.java' 'TODO\(|println\(|printStackTrace\(' "$SOURCE_DIRECTORY" \
-        | grep -Ev ':[0-9]+:[[:space:]]*(//|/\*|\*|\*/)' || true)
+    # Strip comments and quoted literals before checking executable Kotlin/Java calls.
+    matches=$(rg --files -g '*.kt' -g '*.java' "$SOURCE_DIRECTORY" \
+        | while IFS= read -r file; do
+            LC_ALL=C perl -ne '
+                BEGIN {
+                    $block_comment = 0;
+                    $triple_quote = 0;
+                    $line_number = 0;
+                    $double_quote = chr(34);
+                    $single_quote = chr(39);
+                    $backslash = chr(92);
+                }
+                $line_number++;
+                $raw_line = $_;
+                $code = "";
+                for ($index = 0; $index < length($raw_line); ) {
+                    if ($block_comment) {
+                        if (substr($raw_line, $index, 2) eq "*/") {
+                            $block_comment = 0;
+                            $index += 2;
+                        } else {
+                            $index++;
+                        }
+                        next;
+                    }
+                    if ($triple_quote) {
+                        if (substr($raw_line, $index, 3) eq $double_quote x 3) {
+                            $triple_quote = 0;
+                            $index += 3;
+                        } else {
+                            $index++;
+                        }
+                        next;
+                    }
+                    if (substr($raw_line, $index, 2) eq "//") {
+                        last;
+                    }
+                    if (substr($raw_line, $index, 2) eq "/*") {
+                        $block_comment = 1;
+                        $index += 2;
+                        next;
+                    }
+                    if (substr($raw_line, $index, 3) eq $double_quote x 3) {
+                        $triple_quote = 1;
+                        $index += 3;
+                        next;
+                    }
+                    $character = substr($raw_line, $index, 1);
+                    if ($character eq $double_quote || $character eq $single_quote) {
+                        $quote = $character;
+                        $index++;
+                        while ($index < length($raw_line)) {
+                            $character = substr($raw_line, $index, 1);
+                            $index++;
+                            if ($character eq $backslash) {
+                                $index++;
+                                next;
+                            }
+                            last if $character eq $quote;
+                        }
+                        next;
+                    }
+                    $code .= $character;
+                    $index++;
+                }
+                if ($code =~ /(?<![[:alnum:]_])(TODO|println|printStackTrace)\s*\(/) {
+                    print "$ARGV:$line_number:$raw_line";
+                }
+            ' "$file"
+        done || true)
     if [[ -n "$matches" ]]; then
         report_failure "executable TODO, println, or printStackTrace" "$matches"
     fi
